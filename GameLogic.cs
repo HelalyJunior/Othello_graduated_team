35d131a8160cfd6b6223b1da0c4764169376549f
omarHussienMahmoudAli <136204735+omarHussienMahmoudAli@users.noreply.github.com>
2023-06-11 02:24:53 +0300

    Add files via upload

--- GameLogic.cs
+++ GameLogic.cs
@@ -323,6 +323,178 @@ public class GameLogic
         changeTurns();
 
     }
+
+    /*
+        updates the score :)
+    */
+    private void updateScore(Move move)
+    {
+        score[currentPlayer]+=1;
+        /*
+            Update the score
+        */
+        foreach( Position pos in move.outFlanked)
+        {
+            score[currentPlayer]+=1;
+            score[Player.getOpponent(currentPlayer)]-=1;
+        }
+
+
+    }
+
+
+
+    public static Dictionary<Position,List<Position>> getLegalMovesGivenState(PlayerColor[,] state,PlayerColor player)
+    {
+        Dictionary<Position,List<Position>> legalMovesDict = new Dictionary<Position,List<Position>>();
+        for(int i=0;i<8;i++)
+        {
+            for(int j=0; j<8; j++)
+            {
+                Position position = new Position(i,j);
+                if(isMoveLegalGivenState(position,player,out List<Position> outFlanked,state))
+                {
+                    legalMovesDict[position] = outFlanked;
+                }
+            }
+        }
+        return legalMovesDict;
+    }
+
+    public static bool isMoveLegalGivenState(Position position , PlayerColor player , out List<Position> outFlanked,PlayerColor[,] currentState)
+    {
+        if(currentState[position.row,position.column]==PlayerColor.NULL)
+        {
+            outFlanked=getOutFlankedGivenState(player,position,currentState);
+            return outFlanked.Count>0;
+        }
+        else
+        {
+            outFlanked=null;
+            return false;
+        }
+    }
+
+    public static List<Position> getOutFlankedGivenState(PlayerColor player , Position position,PlayerColor[,] currentState)
+    {
+        List<Position> outFlanked = new List<Position>();
+
+        Direction[] directions = new Direction[]
+        {
+            Direction.UP,
+            Direction.DOWN,
+            Direction.LEFT,
+            Direction.RIGHT,
+            Direction.DIAGONALRIGHTUP,
+            Direction.DIAGONALRIGHTDOWN,
+            Direction.DIAGONALLEFTUP,
+            Direction.DIAGONALLEFTDOWN
+        };
+        foreach (Direction direction in directions)
+        {
+           outFlanked.AddRange(getOutFlankedByDirectionGivenState(player,position,direction,currentState));
+        }
+        return outFlanked;
+    }
+
+    public static List<Position> getOutFlankedByDirectionGivenState(PlayerColor player , Position position , Direction direction , PlayerColor[,] state)
+    {
+        int row;
+        int column;
+
+        int shiftInRows=0;
+        int shiftInCols=0;
+
+        switch(direction)
+        {
+            case Direction.UP:
+                shiftInRows=-1;
+                shiftInCols=0;
+                break;
+            case Direction.DOWN:
+                shiftInRows=1;
+                shiftInCols=0;
+                break;
+            case Direction.LEFT:
+                shiftInRows=0;
+                shiftInCols=-1;
+                break;
+            case Direction.RIGHT:
+                shiftInRows=0;
+                shiftInCols=1;
+                break;
+            case Direction.DIAGONALRIGHTUP:
+                shiftInRows=-1;
+                shiftInCols=1;
+                break;
+            case Direction.DIAGONALRIGHTDOWN:
+                shiftInRows=1;
+                shiftInCols=1;
+                break;
+            case Direction.DIAGONALLEFTUP:
+                shiftInRows=-1;
+                shiftInCols=-1;
+                break;
+            case Direction.DIAGONALLEFTDOWN:
+                shiftInRows=1;
+                shiftInCols=-1;
+                break;
+        }
+  
+        List<Position> outFlanked = new List<Position>();
+        row = position.row + shiftInRows;
+        column = position.column + shiftInCols;
+        while(isInBorder(row,column) && state[row,column]!=PlayerColor.NULL)
+        {
+            if(state[row,column]==Player.getOpponent(player))
+            {
+                outFlanked.Add(new Position(row,column));
+                row += shiftInRows;
+                column += shiftInCols;
+            }
+            else // it means that I reached one of my discs
+            {
+                return outFlanked;
+            }
+        }
+        return new List<Position>(); //empty
+    }
+
+    public static PlayerColor[,] takeMoveGivenState( PlayerColor[,] currentState , Move move,PlayerColor player , out PlayerColor turn)
+    {
+        PlayerColor[,] state = (PlayerColor[,])currentState.Clone();
+        state[move.destination.row,move.destination.column]=player;
+
+        /*
+            Flip discs
+        */
+        foreach( Position pos in move.outFlanked)
+        {
+            state[pos.row,pos.column]=player;
+        }
+        turn = changeTurnsGivenState(currentState,player);
+        return state;
+    }
+
+
+    public static PlayerColor changeTurnsGivenState(PlayerColor[,] currentState , PlayerColor currentPlayer)
+    {
+        if(getLegalMovesGivenState(currentState,Player.getOpponent(currentPlayer)).Count>0)
+        {
+            return Player.getOpponent(currentPlayer); //normal
+        }
+
+        return currentPlayer;
+    }
+
+    public static bool isGameOverGivenState(PlayerColor[,] state)
+    {
+        if(getLegalMovesGivenState(state,PlayerColor.BLACK).Count==0 && getLegalMovesGivenState(state,PlayerColor.WHITE).Count==0)
+        {
+            return true;
+        }
+        return false;
+    }
     public GameLogic()
     {
         fillStartingState();
