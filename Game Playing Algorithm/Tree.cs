8a983a0fce1726982abef5a4f463b65e4bf7ff7c
MoazAhmed-eng <85304515+MoazAhmed-eng@users.noreply.github.com>
2023-06-11 19:36:02 +0300

    Created the tree class.
    The tree is the object instantiated at every move to find out the best move.

---
+++ Game Playing Algorithm/Tree.cs
@@ -0,0 +1,19 @@
+public class Tree
+{
+    Node root;
+
+    public Tree()
+    {
+        this.root = null;
+    }
+
+    public Tree(Node root)
+    {
+        this.root = root;
+    }
+
+    public Node getRoot()
+    {
+        return root;
+    }
+}
